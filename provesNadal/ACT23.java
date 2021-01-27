ec314b1a4c3d0dc90f1591a66a4330f43bcf2877
jaume <jfullana@cifpfbmoll.eu>
2021-01-27 22:18:25 +0100

    borrat sobrants

--- provesNadal/ACT23.java
+++
@@ -1,8 +0,0 @@
-public class ACT23 {
-    
-    public static void main(String[] args){
-
-        String frase="La lluvia en Sevilla es una maravilla";
-        System.out.println(frase.replace('a','e'));
-    }
-}
